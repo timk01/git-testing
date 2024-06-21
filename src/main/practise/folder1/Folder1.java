6122f01af9026d9569a92b0ec4bbcf6688c551cc
Timur <timk01@mail.ru>
2024-06-21 12:47:14 +0300

    creating conflict

--- src/main/practise/folder1/Folder1.java
+++ src/main/practise/folder1/Folder1.java
@@ -3,5 +3,6 @@ package practise.folder1;
 public class Folder1 {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Conflicting change");
     }
 }
